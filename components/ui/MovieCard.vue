377e44fec7c1c8960c2b40bdca009c0bef75f099
@hdelamata <hugo.delamata@vts.team>
2024-10-16 11:55:11 +0200

    movie card

---
+++ components/ui/MovieCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card class="mx-auto" max-width="400">
+    <v-img class="align-end text-white" height="200" :src="dataMovie.image" cover>
+      <v-card-title>{{ dataMovie.title }}</v-card-title>
+    </v-img>
+
+    <v-card-subtitle class="pt-4"> {{ dataMovie.rating }} </v-card-subtitle>
+
+    <v-card-actions>
+      <v-btn color="orange" text="Explore"></v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script lang="ts" setup>
+const dataMovie = defineProps<{
+  title: string;
+  image: string;
+  rating: number;
+  link: string;
+}>();
+</script>
